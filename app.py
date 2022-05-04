d7c679c68db1aa4c50b8625809a74b8ce6a3bf59
Shim-JS <s961215@naver.com>
2022-05-04 09:19:13 +0900

    - 연동 완료, 부트스트랩 개선 버젼,

--- app.py
+++ app.py
@@ -59,5 +59,44 @@ def summary():
     return render_template('summary.html', movie_url=movie_url, text_all = text_all,
                            sum_text = sum_text, movie_title = movie_title)
 
+
+@app.route('/summary2',methods = ['POST', 'GET'])
+def summary2():
+    if request.method == 'POST':
+        result = request.form
+        movie_url = result.get('address')
+
+        # 동영상 다운
+        downYoutubeMp3(movie_url)
+
+        # 동영상 이름 추출
+        movie_title = down_title(movie_url)
+        content = movie_title + '.flac'
+
+        # 동영상 path가져오기
+        path = os.getcwd()
+        folder_yt = "yt"
+        contents = os.path.join(path, folder_yt, content)
+
+        # 동영상 스토리지 업로드
+        upload_blob_from_memory("dgu_dsc_stt", contents, content)
+
+        # 동영상 STT
+        # 스토리지 path
+        gcs_url = "gs://dgu_dsc_stt/"
+        gcs_file = gcs_url + content
+        try:
+            text_all = transcribe_gcs(gcs_file, content, 44100)
+        except:
+            text_all = transcribe_gcs(gcs_file, content, 48000)
+        ## summary
+
+        #요약문 생성
+        sum_text = summary_text(text_all,models[0])
+
+        # 임베딩 링크 생성
+        movie_url = movie_url.replace('watch?v=', "embed/")
+    return render_template('summary_2.html', movie_url=movie_url, text_all = text_all,
+                           sum_text = sum_text, movie_title = movie_title)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
--- stt.py
+++ stt.py
@@ -2,7 +2,7 @@ import os, os.path
 from google.cloud import storage
 from google.cloud import speech
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/심종수/Desktop/**********.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/심종수/Desktop/*********.json"
 
 # 스토리지 업로드
 def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
